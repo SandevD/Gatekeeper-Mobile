ee45a13a43bd86b7f90ed5248165460c6b200c0e
SandevD <sandev.net@gmail.com>
2023-12-25 00:21:33 +0530

    🎨Add login page

---
+++ lib/pages/auth/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Login'),
+      ),
+    );
+  }
+}
